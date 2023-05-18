66cc65235a44369752f9c56661eb1336f8faa32f
JBSilvaDev <jbsilva.dev@outlook.com>
2023-05-18 03:41:33 -0300

    new main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import os
 os.environ["WDM_LOG_LEVEL"] = "0"
+os.environ["WDM_HIDE_OUTPUT"] = "true"
 tqdm.monitor_interval = 0
 from view.launcher_tkinter import LauncherTKInter
 from controller.gpoController.gpo_controller import GpoController
